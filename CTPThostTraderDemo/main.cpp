e52391994229b8d458c2cae507e6a83183b5bcaa
Sonny <sonny.frozenpine@gmail.com>
2020-12-25 15:10:23 +0800

    完成 show 参数处理

--- CTPThostTraderDemo/main.cpp
+++ CTPThostTraderDemo/main.cpp
@@ -8,9 +8,83 @@
 #include "ini.h"
 #include "Command.h"
 
+#ifdef _WIN32
+#define STRDUP _strdup
+#else
+#define STRDUP strdup
+#endif
+
 const char flowPath[] = "flow/";
 const std::regex floatPattern("\\d*\\.?\\d+");
 const std::regex intPattern("\\d+");
+
+char* ltrim(char* s) {
+	while (isspace(*s)) s++;
+	return s;
+}
+
+char* rtrim(char* s) {
+	char* back = s + strlen(s);
+	while (isspace(*--back));
+	*(back + 1) = '\0';
+	return s;
+}
+
+char* trim(char* s) {
+	return rtrim(ltrim(s));
+}
+
+char** split(const char* input, int& outCount, char delim = ',') {
+	char** result = 0;
+
+	int count = 0;
+	int len = strlen(input);
+	char* instruments = (char*)malloc(size_t(len) + 1);
+	if (!instruments) {
+		return NULL;
+	}
+	instruments[len] = 0;
+	memcpy(instruments, input, len);
+	char* tmp = instruments;
+	char* last_comma = 0;
+	char delimer[2] = {0};
+	delimer[0] = delim;
+	delimer[1] = 0;
+
+	while (*tmp) {
+		if (delim == *tmp) {
+			count++;
+			last_comma = tmp;
+		}
+
+		tmp++;
+	}
+	// 如果分隔符不存在或者最后一个字符不是逗号分隔符，则计数加一
+	// a,b count = 2
+	// a,b, count = 2
+	if (!last_comma || last_comma < (instruments + strlen(instruments) - 1)) {
+		count++;
+	}
+
+	result = (char**)malloc(sizeof(char*) * count);
+
+	if (result) {
+		int idx = 0;
+
+		char* token = strtok(instruments, delimer);
+
+		while (token != NULL) {
+			*(result + idx++) = STRDUP(trim(token));
+
+			token = strtok(NULL, delimer);
+		}
+
+		outCount = count;
+	}
+
+	free(instruments);
+	return result;
+}
 
 int cmdVersion(void* api, const std::vector<std::string>& args) {
 	printf("Current API version is: %s\n", ((TDUserApi*)api)->GetApiVersion());
@@ -41,7 +115,30 @@ int cmdShow(void* api, const std::vector<std::string>& args) {
 		std::string InstrumentID = "";
 
 		for (int i = 1; i < args.size(); i++) {
-			/*args[i].*/
+			int count = 0;
+			char** results = split(args[i].c_str(), count, '=');
+
+			if (count != 2) {
+				fprintf(stderr, "Invalid args: %s\n", args[i].c_str());
+				continue;
+			}
+
+			if (strcmp(results[0], "ExchangeID") == 0) {
+				ExchangeID = results[1];
+				continue;
+			}
+
+			if (strcmp(results[0], "ProductID") == 0) {
+				ProductID = results[1];
+				continue;
+			}
+
+			if (strcmp(results[0], "InstrumentID") == 0) {
+				InstrumentID = results[1];
+				continue;
+			}
+
+			fprintf(stderr, "Invalid arg key word: %s\n", results[0]);
 		}
 
 		printf("ExchangeID, ProductID, InstrumentID, PriceTick, VolumeMultiple, UnderlyingIns, StrikePrice\n");
@@ -290,7 +387,7 @@ int main(int argc, char* argv[]) {
 	api->ReqQryInstrument(&qryIns);
 	printf("Quering instrument info.\n");
 
-	api->QueryMarginRateAll(brokerID, userID);
+	// api->QueryMarginRateAll(brokerID, userID);
 	// api->QueryCommRateAll(brokerID, userID);
 
 	cli.RunForever();
